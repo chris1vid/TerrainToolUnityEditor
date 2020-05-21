59b947b614038f9456a5b3226f332d683fd6cf16
Christian Anton Sladeczek <chris1vid@gmail.com>
2020-05-21 16:43:11 -0400

    Commited unity files

---
+++ UnityEditorProj/Assets/TerrainToolEditor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TerrainToolEditor : EditorWindow
+{
+
+
+
+    static Terrain[] terrains;
+    static string[] options;
+
+    [MenuItem("Window/Terrain Height Filter")]
+    static void ShowWindow()
+    {
+        terrains = new Terrain[5];
+        options = new string[5];
+
+        TerrainToolEditor window = (TerrainToolEditor)EditorWindow.GetWindow(typeof(TerrainToolEditor));
+        window.minSize = new Vector2(300, 500);
+        window.Show();
+        terrains = Terrain.activeTerrains;
+
+        int n = 0;
+
+
+
+        foreach (Terrain i in terrains)
+        {
+            string nk = i.ToString();
+
+            options[n] = nk;
+            n++;
+        }
+        n = 0;
+    }
+
+
+
+
+    public int index = 0;
+    public int index2 = 0;
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Test");
+        index = EditorGUILayout.Popup(index, options);
+        index2 = EditorGUILayout.Popup(index2, options);
+        if (GUILayout.Button("Accept"))
+        {
+            AddFilter();
+        }
+    }
+
+    void AddFilter()
+    {
+        // We need to have the terrain accessed
+        terrains[index];
+    }
+}
+
+ 
+
+
---
+++ UnityEditorProj/Assets/TextureTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureTest : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class SplatHeights
+    {
+        public int textureIndex;
+        public int startingHeight;
+    }
+
+    public SplatHeights[] splatHeights;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TerrainData terrainData = Terrain.activeTerrain.terrainData;
+        float[,,] splatmapData = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
+
+        for (int y = 0; y < terrainData.alphamapHeight; y++)
+        {
+            for (int x = 0; x < terrainData.alphamapWidth; x++)
+            {
+                float terrainHeight = terrainData.GetHeight(y, x);
+
+                float[] splat = new float[splatHeights.Length];
+
+                for (int i=0; i< splatHeights.Length; i++)
+                {
+                    if (terrainHeight >= splatHeights[i].startingHeight)
+                    {
+                        splat[i] = 1;
+                    }
+
+
+                    for (int j = 0; j < splatHeights.Length; j++)
+                    {
+                        splatmapData[x, y, j] = splat[j];
+                    }
+                }
+            }
+
+            terrainData.SetAlphamaps(0, 0, splatmapData);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
